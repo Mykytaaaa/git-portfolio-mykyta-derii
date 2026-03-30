09275bcfc1d44983e54ce848e527c004be884f25
Mykyta D <nikita.derii@gmail.com>
2026-03-30 21:11:32 +0200

    Created an infinite loop for handling user input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import org.example.Exceptions.InvalidArgumentException;
+import org.example.Exceptions.InvalidOperationException;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+
+        while (true) {
+            String input = in.nextLine();
+
+            Operation operation;
+            try {
+                operation = new Operation(input);
+            } catch (InvalidArgumentException ex) {
+                System.out.println("Invalid input format");
+                continue;
+            }
+
+            int result;
+            try {
+                result = calculator.Calculate(operation);
+            } catch (InvalidOperationException ex) {
+                System.out.println("Unsupported operation");
+                continue;
+            }
+
+            System.out.println(result);
+        }
     }
 }
